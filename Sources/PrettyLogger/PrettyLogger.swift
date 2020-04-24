c130ae09fbe241b4c02321ad0ac5ac369b5e8d0e
Rushit Rakhasiya <avidrush94@gmail.com>
2020-04-24 15:44:57 +0530

    Adding logging functions

--- Sources/PrettyLogger/PrettyLogger.swift
+++ Sources/PrettyLogger/PrettyLogger.swift
@@ -1,3 +1,36 @@
-struct PrettyLogger {
-    var text = "Hello, World!"
+import Foundation
+
+func logMessage(
+    _ message: String,
+    _ file: String = #file,
+    _ line: Int = #line
+) {
+    #if DEBUG
+    let fileString = NSString(string: file).lastPathComponent
+    print("👉🏻 Log [\(fileString):\(line)] 👉🏻 \(message)")
+    #endif
+}
+
+
+func logError(
+    _ message: String,
+    _ file: String = #file,
+    _ line: Int = #line
+) {
+    #if DEBUG
+    let fileString = NSString(string: file).lastPathComponent
+    print("❌ Error [\(fileString):\(line)] ❌ \(message)")
+    #endif
+}
+
+
+func warning(
+    _ message: String,
+    _ file: String = #file,
+    _ line: Int = #line
+) {
+    #if DEBUG
+    let fileString = NSString(string: file).lastPathComponent
+    print("⚠️ Warning [\(fileString):\(line)] ⚠️ \(message)")
+    #endif
 }
